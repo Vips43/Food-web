2fe744002afb39087bfd113ac63d1e7491a97b42
Vips43 <vipulgehlot43@gmail.com>
2025-11-08 10:49:57 +0530

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Route path='/' element={< RandomMeals/> } />
         <Route path='/view_recepie/:mealName' element={ <ViewRecepie />} />
       </Routes>
-    
+  
     </div>
     </>
   )
